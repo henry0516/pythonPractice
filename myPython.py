5bdb7733e0f6ac961fcf288c163905f448152d52
henry0516 <henry-0516@hotmail.com>
2016-03-22 16:54:32 +0800

    myPython
    This is a practice for python

---
+++ myPython.py
@@ -0,0 +1,22 @@
+#C:\Python27\python.exe
+# -*- coding:utf-8 -*-
+
+
+#This is Test Case
+
+'''
+This
+is
+Test
+case
+'''
+
+#this is tutorial 1
+print("Hello, World!")
+
+#this is tutorial 2
+myint = 7
+myfloat = 5.0
+myfloat = float(5)
+
+
